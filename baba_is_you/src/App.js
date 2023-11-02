05a9b6ce4ada0a0e20d848b5170881b883b52855
PetrachkovVasily <vasyapt04@gmail.com>
2023-11-02 10:02:38 +0300

    Added button

---
+++ baba_is_you/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MyButton from "./components/UI/button/MyButton";
+
+function App() {
+  return (
+    <div className="App">
+      <MyButton></MyButton>
+    </div>
+  );
+}
+
+export default App;
---
+++ baba_is_you/src/components/UI/button/MyButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from './MyButton.module.css'
+
+function MyButton(props) {
+    return (
+        <button className={classes.myBtn}>
+            батон
+        </button>
+    )
+}
+
+export default MyButton
