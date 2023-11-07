8dba5a12c7b4876aa3feee5dd10131ec4560b5d0
PetrachkovVasily <vasyapt04@gmail.com>
2023-11-07 11:50:01 +0300

    Button, Input

--- baba_is_you/src/App.js
+++ baba_is_you/src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
+  const [inputBody, setInputBody] = useState('');
+
+  function handleClick(event) {
+    console.log(inputBody);
+  }
+
   return (
     <div className="App">
-      <MyButton></MyButton>
+      <MyButton onClick={handleClick}>
+        Baton
+      </MyButton>
+      <MyInput value={inputBody} onChange={event => setInputBody(event.target.value)} type='text' placeholder='Input for texting'/>
     </div>
   );
 }
--- baba_is_you/src/components/UI/button/MyButton.jsx
+++ baba_is_you/src/components/UI/button/MyButton.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import classes from './MyButton.module.css'
 
-function MyButton(props) {
+function MyButton({children, ...props}) {
     return (
-        <button className={classes.myBtn}>
-            батон
+        <button {...props} className={classes.myBtn}>
+            {children}
         </button>
     )
 }
